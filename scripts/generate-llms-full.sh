cbf5e6143e5691520dff1ab6613cf25fcb610595
Jörg Zimmer <jeden.tag.bin.ich@googlemail.com>
2026-03-20 07:51:35 +0100

    fix: restore umlaut handling in generate-llms-full.sh and fix syntax error

--- scripts/generate-llms-full.sh
+++ scripts/generate-llms-full.sh
@@ -13,24 +13,26 @@ echo -e "# FULL KNOWLEDGE BASE\n" >> "$OUTPUT"
 echo -e "## GLOSSARY\n" >> "$OUTPUT"
 # Process Glossary (Alphabetical)
 for file in $(ls "$GLOSSARY_DIR"/*.md | sort); do
-    TITLE=$(grep "^title:" "$file" | head -1 | sed 's/title: //;s/"//g')
+    TITLE=$(grep "^title:" "$file" | head -1 | sed 's/title: //;s/"//g' | sed 's/ö/oe/g;s/ä/ae/g;s/ü/ue/g;s/Ö/Oe/g;s/Ä/Ae/g;s/Ü/Ue/g;s/ß/ss/g')
     echo -e "### $TITLE\n" >> "$OUTPUT"
-    # Append body content: skip first two frontmatter dashes, strip ALL HTML tags, absolute URLs
+    # Append body content: skip first two frontmatter dashes, strip ALL HTML tags, absolute URLs, replace umlauts
     awk 'BEGIN{c=0} /^---$/ && c<2 {c++; next} c>=2 {print}' "$file" | \
     sed 's/<[^>]*>//g' | \
-    sed -E "s|\]\((/[^)]*)\)|]($DOMAIN\1)|g" >> "$OUTPUT"
+    sed -E "s|\]\((/[^)]*)\)|]($DOMAIN\1)|g" | \
+    sed 's/ö/oe/g;s/ä/ae/g;s/ü/ue/g;s/Ö/Oe/g;s/Ä/Ae/g;s/Ü/Ue/g;s/ß/ss/g' >> "$OUTPUT"
     echo -e "\n---\n" >> "$OUTPUT"
 done
 
 echo -e "## BLOG ARTICLES\n" >> "$OUTPUT"
 # Process Blog (Newest first)
 for file in $(ls "$BLOG_DIR"/*.md | sort -r); do
-    TITLE=$(grep "^title:" "$file" | head -1 | sed 's/title: //;s/"//g')
+    TITLE=$(grep "^title:" "$file" | head -1 | sed 's/title: //;s/"//g' | sed 's/ö/oe/g;s/ä/ae/g;s/ü/ue/g;s/Ö/Oe/g;s/Ä/Ae/g;s/Ü/Ue/g;s/ß/ss/g')
     echo -e "### $TITLE\n" >> "$OUTPUT"
-    # Append body content: skip first two frontmatter dashes, strip ALL HTML tags, absolute URLs
+    # Append body content: skip first two frontmatter dashes, strip ALL HTML tags, absolute URLs, replace umlauts
     awk 'BEGIN{c=0} /^---$/ && c<2 {c++; next} c>=2 {print}' "$file" | \
     sed 's/<[^>]*>//g' | \
-    sed -E "s|\]\((/[^)]*)\)|]($DOMAIN\1)|g" >> "$OUTPUT"
+    sed -E "s|\]\((/[^)]*)\)|]($DOMAIN\1)|g" | \
+    sed 's/ö/oe/g;s/ä/ae/g;s/ü/ue/g;s/Ö/Oe/g;s/Ä/Ae/g;s/Ü/Ue/g;s/ß/ss/g' >> "$OUTPUT"
     echo -e "\n---\n" >> "$OUTPUT"
 done
 
